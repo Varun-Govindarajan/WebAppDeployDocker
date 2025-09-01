e466b89daa903d1c9d39e7af1ea7d789f9d007e3
Varun-Govindarajan <152054074+Varun-Govindarajan@users.noreply.github.com>
2025-09-01 10:30:45 +0530

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-set -e
-
-# Stop old container if exists
-sudo docker stop myapp || true
-sudo docker rm myapp || true
-
-# Build image from Dockerfile in your repo
-sudo docker build -t myimage:latest .
-
-# Run container
-sudo docker run -d -p 80:80 --name myapp myimage:latest
+cd /home/ubuntu/app
+docker build -t simple-web .
+docker run -d -p 80:80 --name simple-web simple-web
