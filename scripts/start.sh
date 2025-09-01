5fb2286d3ffb5603664fce91e301168637f79492
Varun-Govindarajan <152054074+Varun-Govindarajan@users.noreply.github.com>
2025-09-01 10:27:37 +0530

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cd /home/ubuntu/app
-docker build -t simple-web .
-docker run -d -p 80:80 --name simple-web simple-web
+sudo docker stop myapp || true
+sudo docker rm myapp || true
+sudo docker run -d -p 80:80 --name myapp myimage:latest
